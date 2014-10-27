27aae8be006a069cff8c0e9b001cf9be58bf4546
Michael Lin <linmx0130@163.com>
2014-10-27 19:40:34 +0800

    something new

--- src/monopoly/kernel/User.java
+++ src/monopoly/kernel/User.java
@@ -2,15 +2,39 @@
  * Record the data and manage the actions of user.
  */
 package monopoly.kernel;
-
 public class User
 {
-	public static userCount=0;
-	public String name;
-	public int id;
+	public static int userCount=0;
+	private String name;
+	private int id;
+	private int cash;
 	public User(String name)
 	{
 		this.name=name;
-		id=userCount++;
+		this.id=userCount++;
+		this.cash=0;
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+	public int getId()
+	{
+		return this.id;
+	}
+	public int getCash()
+	{
+		return this.cash;
+	}
+	public void modifyCash(int d)
+	{
+		this.cash+=d;
+	}
+	/**
+	 * control() : call it when it's this user's turn
+	 */
+	public void control()
+	{
+
 	}
 }
