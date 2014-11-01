f71363e4a8b64ad66fd96d1b2fbe621fa2a6c1fa
Mengxiao Lin <linmx0130@163.com>
2014-11-01 11:11:41 +0800

    add mortgage land method

--- src/monopoly/kernel/MainController.java
+++ src/monopoly/kernel/MainController.java
@@ -8,13 +8,26 @@ public class MainController
 	public static MapManager map=new MapManager();
 	public static User[] userList;
 	public static int userTotal;
-	
+	public static boolean[] inGame;
+
+	/** buildUsers
+	 *  build users for init game.
+	 */
 	public static void buildUsers(int userTotal)
 	{
 		MainController.userTotal=userTotal;
 		userList=new User[userTotal];
 		BankSystem.savings=new int[userTotal];
 		map.userPosition=new int[userTotal];
+		inGame=new boolean[userTotal];
+		for (int i=0;i<userTotal;++i) inGame[i]=true;;
+	}
+	/** endGame
+	 *  a user give up game
+	 */
+	public static void endGame(int userId)
+	{
+		inGame[userId]=false;
 	}
 	public static void mainLoop() throws Exception
 	{
@@ -24,7 +37,7 @@ public class MainController
 			TimeManager.newDay();
 			for (int i=0;i<userTotal;++i)
 			{
-				userList[i].control();
+				if (inGame[i]) userList[i].control();
 			}
 		}
 	};
--- src/monopoly/ui/LandTradeForm.java
+++ src/monopoly/ui/LandTradeForm.java
@@ -33,7 +33,7 @@ public class LandTradeForm
 						+nowLand.getBuyingPrice()+"元即可购买!");
 		System.out.print("您是否需要购买？(Y->买，否则不买)");
 		String userCommand=cin.next();
-		if (userCommand.equals("Y"))
+		if (userCommand.equals("Y") || userCommand.equals("y"))
 		{
 			if (MainController.userList[userId].getCash()<nowLand.getBuyingPrice())
 			{
@@ -47,7 +47,7 @@ public class LandTradeForm
 		}
 		return false;
 	}
-	/** payArrivedCose
+	/** payArrivedCost
 	 *  the method provided a way to pay arrived cost
 	 */
 	public static void payArrivedCost(int userId,int landId)
@@ -67,11 +67,65 @@ public class LandTradeForm
 		}
 		User nowUser=MainController.userList[userId];
 		while (nowUser.getCash()<nowLand.getArrivedPrice())
-		{//TODO
+		{
+			if (!mortgageLand(userId,true))
+			{
+				break;
+			}
+		}
+		if (nowUser.getCash()<nowLand.getArrivedPrice())
+		{
+			MainController.endGame(userId);
 		}
 		nowUser.modifyCash(-nowLand.getArrivedPrice());
 		MessageManager.showMessage(MessageManager.MESSAGE,"UI::LandTradeForm",
 			nowUser.getName()+"到达"+nowLand.name+"，向地主"+MainController.userList[nowLand.owner].getName()+
 			"支付参观费"+nowLand.getArrivedPrice()+"元");
+	}
+	/** mortgageLand
+	 *  dying -> true means the user is try to deal with his debt
+	 *  return true means finishing a mortgage action
+	 *  return false means give up.
+	 */
+	public static boolean mortgageLand(int userId, boolean dying)
+	{
+		Scanner cin=new Scanner(System.in);
+		System.out.println("请选择您想要出售的土地：");
+		for (int i=0;i<MainController.map.unitTotal;++i)
+		{
+			if (MainController.map.unitList[i].typeId==2)
+			{
+				if (((NormalLand)MainController.map.unitList[i]).owner==userId)
+				{
+					System.out.println("  "+i+"."
+						+((NormalLand)MainController.map.unitList[i]).name+"  -> "
+						+(int)((((NormalLand)MainController.map.unitList[i]).getBuyingPrice()*0.5))
+						+"元");
+				}
+			}
+		}
+		if (dying)
+		{
+			System.out.println("  -1.放弃治疗，认输！");
+		}else
+		{
+			System.out.println("  -1.放弃本次卖地融资");
+		}
+		int choose;
+		do 
+		{		
+			System.out.print("请输入您选择出售土地的编号（数字）:");
+			choose=cin.nextInt();
+			if (choose==-1) return false;
+			if (((NormalLand)MainController.map.unitList[choose]).owner!=userId)
+			{
+				System.out.println("非法选择！请重新选择！");
+				continue;
+			}
+			MainController.userList[userId].modifyCash(
+				(int)(((NormalLand)MainController.map.unitList[choose]).getBuyingPrice()*0.5));
+			((NormalLand)MainController.map.unitList[choose]).owner=-1;
+			return true;
+		}while (true);
 	}
 }
